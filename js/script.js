24d52d0a2903ed99ff2d691b58695311c23b4495
Patrycja Gomza <patrycja.gomza@gmail.com>
2023-08-16 18:22:38 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+{
+    const tasks = [
+        {
+            content: "zrobić pranie",
+            done: false,
+        },
+        {
+            content: "wypić kawę",
+            done: true,
+        }
+    ];
+
+    const render = () => {
+        let htmlString = "";
+        for (const task of tasks) {
+            htmlString += `
+                <li>
+                ${task.content}
+                </li>
+            `;
+        }
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    }
+
+    const init = () => {
+        render();
+    };
+
+    init();
+
+}
