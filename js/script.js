f516f061ee3ba5a9886bb511447e561e14e32f6b
Patrycja Gomza <patrycja.gomza@gmail.com>
2023-08-16 19:26:26 +0200

    adding an action for the form: block submit function in the form and put the fetched values from the form into the task list

--- js/script.js
+++ js/script.js
@@ -26,6 +26,22 @@
 
     const init = () => {
         render();
+
+        const form = document.querySelector(".js-form");
+        form.addEventListener("submit", (event) => {
+            event.preventDefault();
+
+            const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+            if (newTaskContent === "") {
+                return;
+            }
+            tasks.push({
+                content: newTaskContent,
+            });
+
+            render();
+        });
     };
 
     init();
