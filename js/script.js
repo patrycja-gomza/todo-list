6427fe0283b318bf4eabfaf2a4512b0962fd1797
Patrycja Gomza <patrycja.gomza@gmail.com>
2023-08-17 13:11:24 +0200

    adding functions and button for toggle task done action

--- js/script.js
+++ js/script.js
@@ -23,6 +23,11 @@
         render();
     };
 
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
+        render();
+    }
+
     const render = () => {
         let htmlString = "";
         for (const task of tasks) {
@@ -30,6 +35,7 @@
                 <li
                 ${task.done ? " style=\"text-decoration: line-through\"" : ""}
                 >
+                <button class="js-done"> zrobione? </button>
                 <button class="js-remove"> usuń </button>
                 ${task.content}
                 </li>
@@ -38,11 +44,19 @@
         document.querySelector(".js-tasks").innerHTML = htmlString;
 
         const removeButtons = document.querySelectorAll(".js-remove");
+
         removeButtons.forEach((removeButton, index) => {
             removeButton.addEventListener("click", () => {
                 removeTask(index);
             });
+        });
 
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+        toggleDoneButtons.forEach((toggleDoneButton, index) => {
+            toggleDoneButton.addEventListener("click", () => {
+                toggleTaskDone(index);
+            });
         });
     };
 
