bd91de7de659636113cca9bca66ff66867223d61
Patrycja Gomza <patrycja.gomza@gmail.com>
2023-08-17 13:19:57 +0200

    binding events moved to a separate function

--- js/script.js
+++ js/script.js
@@ -26,23 +26,9 @@
     const toggleTaskDone = (taskIndex) => {
         tasks[taskIndex].done = !tasks[taskIndex].done;
         render();
-    }
+    };
 
-    const render = () => {
-        let htmlString = "";
-        for (const task of tasks) {
-            htmlString += `
-                <li
-                ${task.done ? " style=\"text-decoration: line-through\"" : ""}
-                >
-                <button class="js-done"> zrobione? </button>
-                <button class="js-remove"> usuń </button>
-                ${task.content}
-                </li>
-            `;
-        }
-        document.querySelector(".js-tasks").innerHTML = htmlString;
-
+    const bindEvents = () => {
         const removeButtons = document.querySelectorAll(".js-remove");
 
         removeButtons.forEach((removeButton, index) => {
@@ -58,6 +44,25 @@
                 toggleTaskDone(index);
             });
         });
+    };
+
+    const render = () => {
+        let htmlString = "";
+        for (const task of tasks) {
+            htmlString += `
+                <li
+                ${task.done ? " style=\"text-decoration: line-through\"" : ""}
+                >
+                <button class="js-done"> zrobione? </button>
+                <button class="js-remove"> usuń </button>
+                ${task.content}
+                </li>
+            `;
+        };
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        bindEvents();
     };
 
     const onFormSubmit = (event) => {
